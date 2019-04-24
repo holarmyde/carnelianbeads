451519dec6a5ad385a956d722ab2e264ea308741
Oluwafemi Aremu <ooa255@nyu.edu>
2019-04-24 10:29:32 -0400

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("index");
 });
 
 const port = process.env.PORT || 5000;
